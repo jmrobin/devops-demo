d9104ae61c855ce253757014b7d46f1a2bf11196
Jean-Marc Robin <jmr33bx@yahoo.fr>
2017-11-19 18:32:24 +0100

    R-1.2-Add Log in welcome controller

--- src/main/java/jmr/nand/nanddevops/WelcomeController.java
+++ src/main/java/jmr/nand/nanddevops/WelcomeController.java
@@ -1,5 +1,7 @@
 package jmr.nand.nanddevops;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,12 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomeController
 {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Value( "${spring.profiles.active}" )
 	private String profile;
 
 	@RequestMapping( "/" )
 	public String welcome( Model model )
 	{
+		logger.info("processing index request in welcome controller - enjoy");
 		model.addAttribute("name", "Nand K.");
 		model.addAttribute("profile", profile);
 		return "index";
