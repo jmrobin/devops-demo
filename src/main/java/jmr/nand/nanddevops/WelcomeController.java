d00c29871e9db61456271f08c3b30fde69d8a719
Jean-Marc Robin <jmr33bx@yahoo.fr>
2017-11-19 12:44:02 +0100

    R-1.2-Display profile in index

--- src/main/java/jmr/nand/nanddevops/WelcomeController.java
+++ src/main/java/jmr/nand/nanddevops/WelcomeController.java
@@ -1,5 +1,6 @@
 package jmr.nand.nanddevops;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomeController
 {
+	@Value( "${spring.profiles.active}" )
+	private String profile;
+
 	@RequestMapping( "/" )
 	public String welcome( Model model )
 	{
 		model.addAttribute("name", "Nand K.");
+		model.addAttribute("profile", profile);
 		return "index";
 	}
 }
