4493a691ddb28670fc615a201095e2b036d800a3
Jean-Marc Robin <jmr33bx@yahoo.fr>
2017-11-20 18:13:53 +0100

    Add Groovy file for Jenkins

---
+++ jenkins.groovy
@@ -0,0 +1,22 @@
+node {
+    stage 'prepare'
+    git url: 'git@github.com:jmrobin/devops-demo.git', branch: 'develop'
+
+    stage 'build'
+    sh 'mvn clean package'
+
+    stage 'unit test'
+    junit '**/target/surefire-reports/TEST-*.xml'
+
+    stage 'archivage'
+    archiveArtifacts 'target/*.?ar'
+
+    stage 'integration test'
+    sh 'mvn clean verify'
+
+    stage 'sonar'
+    sh 'mvn sonar:sonar'
+
+    # stage 'deploy'
+    # sh 'mvn deploy'
+}
